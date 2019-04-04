3c650c5d3a71166dec4256f984baeb743b58943b
fherna4157 <fherna4157@aol.com>
2019-04-04 11:21:05 -0400

    Update data_fetcher.py

--- data_fetcher.py
+++ data_fetcher.py
@@ -7,7 +7,8 @@ import os
 import pandas as pd
 import random
 import time
-!pip install urllib2
+
+!{sys.executable} -m pip install urllib2
 import urllib2
 
 from BeautifulSoup import BeautifulSoup
